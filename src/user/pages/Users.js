cab0e91e280c8945cebe03a7de69cc974323088d
Gabarros <gabarros17@gmail.com>
2019-12-12 17:14:22 -0300

    create user item and user list components

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
+import UsersList from '../components/UsersList';
+
 const Users = () => {
-  return <h2>Users Works!</h2>;
+
+  const USERS = [{
+    id: 'u1',
+    name: 'Gabriel',
+    image: '',
+    places: 3
+  }];
+
+  return (
+    <UsersList items={USERS} />
+  );
 };
 
 export default Users;
